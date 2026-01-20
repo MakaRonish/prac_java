e312c06122c85b0f3fd5ef45f6dd24e4e81c7a87
MakaRonish <ronishmakaju12345@gmail.com>
2026-01-19 21:16:11 -0500

    back

---
+++ crash_c/HelloWorld.java
@@ -0,0 +1,11 @@
+package crash_c;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int age = 30;
+        System.out.println(age);
+
+    }
+
+}
